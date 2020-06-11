fee6f421f519b38f0baad3a620fde6c14aa0ad76
mayayo1989banan <66682613+mayayo1989banan@users.noreply.github.com>
2020-06-12 00:42:06 +0300

    Add files via upload

---
+++ гл1с/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+    public static void main(String[] args) { Scanner in = new Scanner(System.in);
+        System.out.print("Input a number of numbers: "); int num = in.nextInt();
+
+        int nums[] = new int[num];
+
+        for (int i = 0; i < num; i++)
+            nums[i]= in.nextInt();
+        System.out.printf("Numbers divisibe by 5 and 7: ");
+        for (int i = 0; i < num; i++)
+            if (nums[i]%5==0&&nums[i]%7==0)
+                System.out.printf("%d ", nums[i]);
+
+        in.close(); }
+
